726d7b2d45e337e2a571367ebace63a0045a016d
User <User@LAPTOP-128AKL1H>
2021-06-10 23:17:13 -0600

    MUCHO CAMBIO

--- Proyecto-Algo/src/Main/FXMLDeEnroll2Controller.java
+++ Proyecto-Algo/src/Main/FXMLDeEnroll2Controller.java
@@ -21,6 +21,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,6 +47,19 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  * FXML Controller class
@@ -217,7 +231,7 @@ public class FXMLDeEnroll2Controller implements Initializable {
 
     //Botón para finalizar el proceso de matrícula
     @FXML
-    private void btn_EndDeEnrollment(ActionEvent event) {
+    private void btn_EndDeEnrollment(ActionEvent event) throws ListException {
         
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Ventana de Confirmación");
@@ -229,6 +243,56 @@ public class FXMLDeEnroll2Controller implements Initializable {
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == buttonTypeYes) {
+            Properties propiedad = new Properties();
+            propiedad.put("mail smtp host", "smtp gmail com");
+            propiedad.put("mail smtp port", "587");
+            propiedad.put("mail.smtp.auth", "true");
+            propiedad.put("mail.smtp.starttls.enable", "true");
+            propiedad.put("mail.smtp.user", "anthony.rs02@gmail.com");
+            propiedad.put("mail.smtp.clave", "");
+
+            Session sesion = Session.getDefaultInstance(propiedad);
+
+            String correoEnvia = "anthony.rs02@gmail.com";
+            String contraseña = "18702NACE";
+            String destinatario = this.student.getEmail();
+
+            MimeMessage mail = new MimeMessage(sesion);
+
+            try {
+//                mail.setFrom(new InternetAddress(correoEnvia));
+                mail.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
+
+                mail.setSubject(asunto());
+                Multipart multipart = new MimeMultipart();
+
+                MimeBodyPart message1 = new MimeBodyPart();
+                String htmlText = "<img src=\"cid:image\">";
+                message1.setContent(htmlText, "text/html");
+
+                MimeBodyPart message2 = new MimeBodyPart();
+                DataSource source = new FileDataSource("C:\\Users\\User\\OneDrive\\Escritorio\\Algoritmos y Estructuras de Datos\\logoBueno.png");
+                message2.setDataHandler(new DataHandler(source));
+                message2.setHeader("Content-ID", "<image>");
+//                message1.setFileName(source.getName());
+
+                MimeBodyPart message3 = new MimeBodyPart();
+                message3.setText(mensaje());
+
+                multipart.addBodyPart(message1);
+                multipart.addBodyPart(message2);
+                multipart.addBodyPart(message3);
+                mail.setContent(multipart);
+                Transport transporte = sesion.getTransport("smtp");
+                transporte.connect("smtp.gmail.com", correoEnvia, contraseña);
+                transporte.sendMessage(mail, mail.getAllRecipients());
+                transporte.close();
+
+            } catch (AddressException ex) {
+                Logger.getLogger(FXMLAddStudentController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (MessagingException ex) {
+                Logger.getLogger(FXMLAddStudentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
             tV_DeEnrollCourse.setVisible(false);
             txtMessage.setVisible(false);
             ComboBox_Course.setVisible(false);
@@ -368,6 +432,35 @@ public class FXMLDeEnroll2Controller implements Initializable {
         } else {
             display();
         }
+
+    }
+    private String asunto() {
+        String result = "Retiro de cursos del estudiante";
+        return result;
+    }
+
+    private String mensaje() throws ListException {
+        String result = "\n";
+        result += "Nombre: " + this.student.getFirstname() + " " + this.student.getLastname() + "\n";
+        result += "Carnet: " + this.student.getStudentID() + "\n";
+        result += "Cursos Retirados:\n";
+        Node aux = this.deEnrollment.getNode(1);
+        while (aux != this.deEnrollment.getNodeLast()) {
+            DeEnrollment tem = (DeEnrollment) aux.data;
+            if (util.Utility.equals(this.student.getId(), tem.getId())) {
+                result += "Curso: " + tem.getCourseID().getName() + " Horario: " + tem.getSchedule() + " Carrera: " + tem.getCourseID().getCareerID().getDescription() + "Motivo del retiro: "+tem.getRemark()+"\n";
+                
+            }
+
+            aux = aux.next;
+        }
+        DeEnrollment tem = (DeEnrollment) aux.data;
+        if (util.Utility.equals(this.student.getId(), tem.getId())) {
+            result += "Curso: " + tem.getCourseID().getName() + " Horario: " + tem.getSchedule() + " Carrera: " + tem.getCourseID().getCareerID().getDescription()  + "Motivo del retiro: "+tem.getRemark()+ "\n";
+            
+        }
+        
+        return result;
 
     }
 }
